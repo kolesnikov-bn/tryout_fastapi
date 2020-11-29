5dcbf0b05ddb34df69f6b142cf644ab1b34a20d7
Boris <kolesnikov.bn@yandex.ru>
2020-11-29 14:00:25 +0300

    Сделана проверка прав доступа для работы с группами товаров
    - Пока что сделано как явный костыль, для проверки поведения прав. Поставлена пометка TODO

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import sys
-
 import uvicorn
-from fastapi import FastAPI
-from loguru import logger
+from fastapi import FastAPI, Depends
+from starlette.requests import Request
 from tortoise.contrib.fastapi import register_tortoise
 
 import settings
-from src import routers
+from src import routers, is_group_allowed
+from src.auth.jwt import get_current_user
 from src.auth.router import auth_router
+from src.user.schemas import UserSchema
 
-logger.add(
-    sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>"
-)
 app = FastAPI(title="FastAPI example")
 
-app.include_router(routers.api_router, prefix=settings.API)
+
+async def check_permissions(
+    request: Request,
+    user: UserSchema = Depends(get_current_user),
+):
+    # TODO: Изменить проверку прав доступа к группе. Сейчас это костыль чтобы получить требуемое поведение.
+    group_id = request.path_params.get("group_id")
+
+    if group_id:
+        await is_group_allowed(int(group_id), user)
+
+    return True
+
+
+app.include_router(
+    routers.api_router,
+    prefix=settings.API,
+    dependencies=[Depends(check_permissions)],
+)
 app.include_router(auth_router, tags=["Auth"])
 
 register_tortoise(
--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,17 @@
-import sys
-
 from fastapi import HTTPException
-from loguru import logger
 from starlette import status
+from tortoise.query_utils import Q
 
 from src.constants import GroupId
+from src.models import (
+    Permission,
+    UserGroupPermission,
+    User,
+    Group,
+    Product,
+)
 from src.user.schemas import UserSchema
 
-logger.add(
-    sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>"
-)
 forbidden_exception = HTTPException(
     status_code=status.HTTP_403_FORBIDDEN, detail="Access is denied"
 )
@@ -18,7 +20,11 @@ forbidden_exception = HTTPException(
 async def is_group_allowed(group_id: GroupId, user: UserSchema) -> bool:
     if user.permissions:
         allowed_groups_id = [x.group.id for x in user.permissions]
-        if group_id not in allowed_groups_id:
+        derived_groups_id = await Group.filter(
+            Q(parent_id__in=allowed_groups_id) | Q(id__in=allowed_groups_id)
+        ).values_list("id", flat=True)
+
+        if group_id not in derived_groups_id:
             raise forbidden_exception
     else:
         raise forbidden_exception
